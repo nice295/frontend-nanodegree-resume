93c441def17c11d7caff32794411468722b6e896
nice295 <nice295@gmail.com>
2015-10-01 23:11:41 +0900

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,30 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var education = {
+	"schools" : [
+		{
+			"name" : "Ajou Univ.",
+			"city" : "Suwon",
+			"degree" : "BA",
+			"major" : "Elecronics"
+		},
+		{
+			"name" : "Sungkyunkwan Univ.",
+			"city" : "Suwon",
+			"degree" : "Master",
+			"major" : "Computer Engineering"
+
+		}
+	]
+};
+
+var skill = [
+	"Coding", "UX Design", "Speech"
+];
+
+console.log(skill.length);
+
+if (skill.length > 0) {
+	$("#header").append(HTMLskillsStart);
+}
+$("#header").append(HTMLskillsStart);
+$("#header").append("111");
+
